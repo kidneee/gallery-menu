fe122dd0ff5089499179e7c98b2c35d1e3e0c156
kido <skido627@gmail.com>
2025-01-22 13:47:01 +0900

    JavaScript挙動調整

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,16 +1,16 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
   /*=================================================
   ハンバーガ―メニュー
   ===================================================*/
-  // ハンバーガーメニューをクリックした時
-  $('.hamburger').on('click', function () {
-    // ハンバーガーメニューの共通処理を呼び出す
-    hamburger();
-  });
-  // メニューのリンクをクリックした時
-  $('#navi a').on('click', function () {
-    // ハンバーガーメニューの共通処理を呼び出す
-    hamburger();
+  const hamburgerButton = document.querySelector('#js-buttonHamburger');
+  const drawerMenu = document.getElementById('drawerMenu');
+  const header = document.querySelector('.header');
+
+  hamburgerButton.addEventListener('click', () => {
+    const isExpanded = hamburgerButton.getAttribute('aria-expanded') === 'true';
+    hamburgerButton.setAttribute('aria-expanded', !isExpanded);
+    drawerMenu.classList.toggle('open');
+    header.classList.toggle('open');
   });
 
   /*=================================================
@@ -33,11 +33,19 @@ $(function () {
   /*=================================================
   フェード表示
   ===================================================*/
-  $('.inview').on('inview', function (event, isInView) {
-    if (isInView) {
-      // 要素（inviewクラス）が表示されたらshowクラスを追加する
-      $(this).stop().addClass('show');
-    }
+
+  let elements = document.querySelectorAll('.fade-in');
+
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  elements.forEach((element) => {
+    observer.observe(element);
   });
 
   /*=================================================
@@ -58,12 +66,12 @@ $(function () {
     // スクロール位置が520pxを超えた場合
     if (scroll > 520) {
       // ロゴとハンバーガ―メニュをfadeInで表示する
-      $('.logo').fadeIn(500);
+      $('.header__logo').fadeIn(500);
       $('.hamburger').fadeIn(500);
       // スクロール位置が520px未満の場合
     } else {
       // ロゴとハンバーガ―メニュをfadeOutで非表示にする
-      $('.logo').fadeOut(500);
+      $('.header__logo').fadeOut(500);
       $('.hamburger').fadeOut(500);
     }
 
@@ -72,28 +80,28 @@ $(function () {
     ===================================================*/
     // 画面下から#galleryまでの距離を取得
     let gallery_position = $('#gallery').offset().top - $(window).height();
-    // 画面下から#accessまでの距離を取得
-    let access_position = $('#access').offset().top - $(window).height();
+    // 画面下から.access__headingまでの距離を取得
+    let access_position = $('.access__heading').offset().top - $(window).height();
 
     // window.innerWidthで画面幅を取得
     // PC表示の場合の処理（画面幅が900pxより大きい場合　※900pxはCSSのブレークポイントとあわせる）
     if (window.innerWidth > 900) {
       // #galleryが表示された場合（スクロール位置が、画面下から#galleryまでの距離を超えた場合）
       if (scroll > gallery_position) {
-        // #accessが表示されるまでの間は、#side-btnを横からスライドさせて表示する
+        // .access__headingが表示されるまでの間は、.side-btnを横からスライドさせて表示する
         if (scroll < access_position) {
-          $('#side-btn').css({
+          $('.side-btn').css({
             transform: 'rotate(-90deg) translateY(0)',
           });
-          // #accessが表示されたら、#side-btnをスライドさせて非表示にする
+          // .access__headingが表示されたら、.side-btnをスライドさせて非表示にする
         } else {
-          $('#side-btn').css({
+          $('.side-btn').css({
             transform: 'rotate(-90deg) translateY(60px)',
           });
         }
-        // #galleryが表示される前は、#side-btnをスライドさせて非表示にする
+        // #galleryが表示される前は、.side-btnをスライドさせて非表示にする
       } else {
-        $('#side-btn').css({
+        $('.side-btn').css({
           transform: 'rotate(-90deg) translateY(60px)',
         });
       }
@@ -109,14 +117,14 @@ $(function () {
     if (scroll > access_position) {
       // #contactが表示されるまでの間は、背景画像をfadeInで表示する
       if (scroll < contact_position) {
-        $('.bg').fadeIn(500);
+        $('.access__bg').fadeIn(500);
       } else {
-        $('.bg').fadeOut(500);
+        $('.access__bg').fadeOut(500);
       }
       // #accessが表示される前の場合
     } else {
       // 背景画像を表示しない
-      $('.bg').fadeOut(500);
+      $('.access__bg').fadeOut(500);
     }
   });
 
@@ -135,25 +143,6 @@ $(function () {
 });
 
 /*=================================================
-ハンバーガ―メニュー（共通処理）
-===================================================*/
-// ハンバーガーメニューをクリックした時とメニュー内のリンクをクリックした時の
-// 処理が同じなので処理を共通化する
-function hamburger() {
-  // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
-  // 存在しない場合を追加する処理を自動で行ってくれる
-  $('.hamburger').toggleClass('active');
-
-  if ($('.hamburger').hasClass('active')) {
-    // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
-    $('#navi').addClass('active');
-  } else {
-    // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
-    $('#navi').removeClass('active');
-  }
-}
-
-/*=================================================
 メインビジュアルの拡大・縮小（共通処理）
 ===================================================*/
 function mv_scale(scroll) {
@@ -162,14 +151,14 @@ function mv_scale(scroll) {
   if (window.innerWidth > 900) {
     // メインビジュアルのCSS（width）を変更する
     // widthの値をスクロール量にあわせて増やすことで画像を拡大させる
-    $('#mainvisual img').css({
+    $('.main-visual__image').css({
       width: 100 / 3 + scroll / 10 + '%',
     });
     // スマホ表示の場合の処理（画面幅が900px以下の場合）
   } else {
     // メインビジュアルのCSS（width）を変更する
     // widthの値をスクロール量にあわせて減らすことで画像を縮小させる
-    $('#mainvisual img').css({
+    $('.main-visual__image').css({
       width: 100 - scroll / 10 + '%',
     });
   }
